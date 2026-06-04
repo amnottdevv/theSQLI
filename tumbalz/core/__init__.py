f73ba7e676e48a3c36b362284c29ddeacad94577
raw.api.github <alzrwr111@gmail.com>
2026-06-04 09:35:58 +0700

    Delete tumbalz directory

--- tumbalz/core/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-ZAMZZZ SQLi Toolkit - Core Modules
-Berisi modul utama: detector, enumerator, dumper, param_scanner, recon_subdomain, recon_path
-"""
--- tumbalz/lib/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-ZAMZZZ SQLi Toolkit - Library Modules
-Berisi modul bantuan: requester, savers, utils
-"""
